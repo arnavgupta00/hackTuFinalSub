3c0f83000a7ce2b5f1c6891e33a77114366993c9
arnavgupta00 <arnavguptagg@gmail.com>
2024-02-10 07:41:16 +0530

    deplyoing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import "dotenv/config";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy:{
-      '/api': 'http://localhost:5000'
+      '/api': process.env.REACT_APP_API_URL ||'http://localhost:5000'
     }
   },
   plugins: [react()],
